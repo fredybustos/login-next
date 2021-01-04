6496736d964ff2d4dbb97ff5a3ae4e3e1ccc9f7e
fredy bustos <afredybustos@gmail.com>
2021-01-03 23:47:20 -0500

    update ignore

--- firebase/client.js
+++ firebase/client.js
@@ -1,5 +1,5 @@
 import firebase from "firebase"
-import { firebaseConfig } from ".firebase.config"
+import { firebaseConfig } from "../config"
 
 !firebase.apps.length && firebase.initializeApp(firebaseConfig)
 
