974af07da4a3c75696500481fc8db7985fb85ccf
fredy bustos <afredybustos@gmail.com>
2021-01-04 00:10:46 -0500

    update config

--- config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBHmXyM3TffnTWvDPyHLioVTyjq7zZbM4s",
-  authDomain: "dev-login-cd302.firebaseapp.com",
-  projectId: "dev-login-cd302",
-  storageBucket: "dev-login-cd302.appspot.com",
-  messagingSenderId: "901375545609",
-  appId: "1:901375545609:web:66e88577005045724dc3d8",
-  measurementId: "G-702JKFCKSR",
-}
